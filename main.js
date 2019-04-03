83770b1cd23b856211fc83536eb64d9f594578b9
junh4533 <junh4533@gmail.com>
2019-04-03 00:33:12 -0400

    fix shifitng text

--- main.js
+++ main.js
@@ -1,64 +1,61 @@
 $(document).ready(function () {
   new fullpage('#fullpage', {
     //options here
-    autoScrolling:true,
+    autoScrolling: true,
     scrollHorizontally: true
   });
   //methods
   // fullpage_api.setAllowScrolling(false);
 
-    var line1 = document.getElementById('year');
-   
-    var typewriter = new Typewriter(line1, {
-      loop: true
-    });
-   
-    typewriter.typeString('freshman')
-      .pauseFor(250)
-      .deleteAll()
-      .pauseFor(250)
-      .typeString('sophomore')
-      .pauseFor(250)
-      .deleteAll()
-      .pauseFor(250)
-      .typeString('junior')
-      .pauseFor(250)
-      .deleteAll()
-      .pauseFor(250)
-      .typeString('senior')
-      .pauseFor(250)
-      .deleteAll()
-      .pauseFor(250)
-      .typeString('student')
-      .pauseFor(250)
-      .start();
-   
-    var line2 = document.getElementById('goal');
-   
-    var typewriter2 = new Typewriter(line2, {
-      loop: true
-    });
-   
-    typewriter2.typeString('learn')
-      .pauseFor(500)
-      .deleteAll()
-      .pauseFor(250)
-      .typeString('grow')
-      .pauseFor(1250)
-      .deleteAll()
-      .pauseFor(250)
-      .typeString('achieve')
-      .pauseFor(250)
-      .deleteAll()
-      .pauseFor(250)
-      .typeString('succeed')
-      .pauseFor(250)
-      .deleteAll()
-      .pauseFor(250)
-      .typeString('<strong style="color:orange">Ascend</strong>')
-      .pauseFor(250)
-      .start();
+  var line1 = document.getElementById('year');
+
+  var typewriter = new Typewriter(line1, {
+    loop: true
+  });
+
+  typewriter.pauseFor(1000).typeString('freshman')
+    .pauseFor(250)
+    .deleteAll()
+    .pauseFor(250)
+    .typeString('sophomore')
+    .pauseFor(250)
+    .deleteAll()
+    .pauseFor(250)
+    .typeString('junior')
+    .pauseFor(250)
+    .deleteAll()
+    .pauseFor(250)
+    .typeString('senior')
+    .pauseFor(250)
+    .deleteAll()
+    .pauseFor(250)
+    .typeString('student')
+    .pauseFor(1500)
+    .start();
+
+  var line2 = document.getElementById('goal');
+
+  var typewriter2 = new Typewriter(line2, {
+    loop: true
+  });
+
+  typewriter2.pauseFor(1000).typeString('learn')
+    .pauseFor(500)
+    .deleteAll()
+    .pauseFor(250)
+    .typeString('grow')
+    .pauseFor(1250)
+    .deleteAll()
+    .pauseFor(250)
+    .typeString('achieve')
+    .pauseFor(250)
+    .deleteAll()
+    .pauseFor(250)
+    .typeString('succeed')
+    .pauseFor(250)
+    .deleteAll()
+    .pauseFor(250)
+    .typeString('<strong style="color:orange">Ascend</strong>')
+    .pauseFor(1500)
+    .start();
 });
-   
-   
-   
