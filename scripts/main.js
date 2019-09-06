50c9c644c760efff9b2ee51b64396158806d3f85
junh4533 <junh4533@gmail.com>
2019-09-06 01:28:48 -0400

    use page piling and AOS

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,23 @@ function opacity_range(val, max, min) {
 
 $(document).ready(function () {
 
-  $('#fullpage').fullpage();
+  //init fagefiling
+  $('#pagepiling').pagepiling({
+    scrollingSpeed: 1400,
+    onLeave: function () {
+      $('.section.active [data-aos]').each(function () {
+        $('#test').addClass("aos-animate")
+        console.log("hello")
+      });
+    }
+  });
+
+  // global configuration for AOS
+  AOS.init({
+    delay: 800,
+    duration: 800, // values from 0 to 3000, with step 50ms
+    easing: 'ease-in-out-cubic',
+  });
 
   function viewport_query(viewport) {
     if (viewport.matches) { // viewport width > 1200px (not mobile device)
@@ -39,7 +55,7 @@ $(document).ready(function () {
         tint_opacity2 = opacity_range(event.clientX, width, 0) * .7;
         tint_long_line.style.background = `rgba(0, 0, 0, ${tint_opacity2})`;
         tint_qronus.style.background = `rgba(0, 0, 0, ${tint_opacity1})`;
-        
+
         grayscale_qronus = opacity_range(event.clientX, width, 0) * 100 + '%';
         grayscale_line = opacity_range(event.clientX, 0, width) * 100 + '%';
         with_qronus_img.style.filter = `grayscale(${grayscale_qronus})`;
