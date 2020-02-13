a23493582e1a7b8a1f2230408c9c00a1b7c63757
junh4533 <junh4533@gmail.com>
2020-02-13 17:44:18 -0500

    add new images

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+  AOS.init({
+    // Global settings:
+    container: "#how-does-it-work",
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 50, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease-in-out', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: true, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'bottom-bottom', // defines which position of the element regarding to window should trigger the animation
+
+  });
+
+  // $('#how-does-it-work').scroll(function () {
+  //   AOS.refresh();
+  // });
+
+});
